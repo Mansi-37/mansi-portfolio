0dcce67a34ba91d70a157ac8acdddbfd4627392a
Mansi-37 <mansisalunke2706@gmail.com>
2025-07-05 18:21:35 +0530

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+// firebase-messaging-sw.js
+
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA7kKNEC3mQIXrI_DLDTFvJBF_5QD3i5Xw",
+  authDomain: "mansi-pwa.firebaseapp.com",
+  projectId: "mansi-pwa",
+  messagingSenderId: "1096969535516",
+  appId: "1:1096969535516:web:b1a1ce87503f66e5f7dfd9"
+});
+
+const messaging = firebase.messaging();
